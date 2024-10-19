04075ee6423c5ef5c42d4917888c0f0380798ad9
lsmmega <pogmegalsm@outlook.com>
2024-10-19 23:01:58 +0800

    create large wpn capsule

--- build.bat
+++ build.bat
@@ -1,8 +1,9 @@
 bmp2nes gfx/obj/wpn/mega_buster.bmp
-bmp2nes gfx/obj/capsule/small_life_capsule.bmp
 bmp2nes gfx/obj/capsule/small_wpn_capsule.bmp
 bmp2nes gfx/obj/bar.bmp
 bmp2nes gfx/obj/tank/e_tank.bmp
 bmp2nes gfx/obj/capsule/large_life_capsule.bmp
+bmp2nes gfx/obj/capsule/small_life_capsule.bmp
+bmp2nes gfx/obj/capsule/large_wpn_capsule.bmp
 asm6 main.asm rm4.nes
 @pause
